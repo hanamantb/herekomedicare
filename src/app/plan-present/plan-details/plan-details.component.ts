01532caabb59b51c248db7cabeedf4c86b50336e
abufazil.mavilampadi <abufazil.mavilampadi@SPTVMLAP-2526>
2023-07-28 19:37:36 +0530

    plan details page and cart design

--- src/app/plan-present/plan-details/plan-details.component.ts
+++ src/app/plan-present/plan-details/plan-details.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plan-details.component.css']
 })
 export class PlanDetailsComponent implements OnInit {
+  panelOpenState = false;
 
   constructor() { }
 
