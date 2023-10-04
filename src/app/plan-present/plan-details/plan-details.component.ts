93cfaaffe3a998f874801e2a971f62a3881fc34b
Davidson Daniel <davidson.daniel@speridian.com>
2023-10-04 23:55:22 +0530

    key benefits

--- src/app/plan-present/plan-details/plan-details.component.ts
+++ src/app/plan-present/plan-details/plan-details.component.ts
@@ -9,7 +9,11 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class PlanDetailsComponent implements OnInit {
   panelOpenState = false;
   details:any
-
+  deductible:any
+  maxOOP:any
+  drugDeductible:any
+  keyBenefits:any=[];
+  keyBenefitValue:any
   constructor(private route: Router) {
     // this.details= this.route.getCurrentNavigation()?.extras.state;
   }
@@ -19,6 +23,26 @@ const detail = sessionStorage.getItem('plandetail')
     if (detail) {
       this.details = JSON.parse(detail);
     }
+    let keyBenefits:[];
+    keyBenefits = this.details.attributes['Key_Benefits']
+    console.log(keyBenefits,'keyBenefits')
+    for(const kb of keyBenefits){
+      this.keyBenefitValue = kb;
+      let array: any = {
+        "apiParameter":this.keyBenefitValue.apiParameter,
+        "displayValue":this.keyBenefitValue.displayValue,
+        "attributeName":this.keyBenefitValue.attributeName
+      }
+      this.keyBenefits.push(array);      
+    }
+    this.keyBenefits.forEach((value:any) => {
+        if(value.apiParameter === 'annual_deductible'){
+          this.deductible = value.displayValue
+        }
+        if(value.apiParameter === 'maximum_oopc'){
+          this.maxOOP = value.displayValue
+        }
+    })
   }
   closePlanDetails() {
     window.close();
